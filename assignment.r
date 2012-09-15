edefb5d1bb561a92b9a0d9c21bdaddae268c4775
traminer <gilbert.ritschard@unige.ch>
2012-09-15 22:51:49 +0300

    Update assignment.r
    Answers to your questions inside the code.

--- assignment.r
+++ assignment.r
@@ -29,6 +29,8 @@ if(biofam$sex="woman") min(biofam$age)
 ### It works, but it this the best/most direct way to do it?
 summary(biofam[biofam$sex=="woman",]$age)
 
+#### GR: Yes, in R it is much more efficient to work with conditional indexing rather than using loops and ifs.
+
 # Add a cohort factor to the biofam data frame grouping the birth years 
 # into the following categories: 1900-1929, 1930-1939, 1940-1949, 1950-1959.
 cohort <- cut(biofam$birthyr, c(1900, 1930, 1940, 1950, 1960), labels = c("1900-1929", "1930-1939", "1940-1949", "1950-1959"), right = FALSE)
@@ -36,9 +38,15 @@ cohort <- cut(biofam$birthyr, c(1900, 1930, 1940, 1950, 1960), labels = c("1900-
 ### QUESTION ###
 ### How to add that factor to the dataframe?
 
+#### GR: Just assign it to  biofam$cohort as you did for the variable age.
+biofam$cohort <- cohort
+
+
 # Generate an histogram of the distribution of birthyear 
 # using the above birth year classes.
 hist(biofam$birthyr, breaks = "cohort")
+
+## "cohort" is a string! You should give: breaks=c(1900, 1930, 1940, 1950, 1960)
 
 ### QUESTION ###
 ### Produces the following error message:
@@ -55,6 +63,8 @@ crosstab <- table (cohort, biofam$a25)
 ### "all arguments must have the same length"
 ### I would assume this is because "cohort" is not part of the dataset right now?
 
+#### No, this would not change the length of cohort. It works for me when I run your code
+
 # Fit a logistic regression for the probability to be married with a child 
 # and having left home at 25 years old in terms of the language of the 
 # questionnaire and the sex.
@@ -68,3 +78,6 @@ biofam.lg.gr <- glm (a25==6 ~ plingu02 + sex, family = binomial, data = biofam[c
 ### Produces the following error message:
 ### "undefined columns selected"
 ### I would assume this is because "cohort" is not part of the dataset right now?
+
+#### gr: No! biofam is a data frame: it has two dimensions, you have to specify which columns you want.
+#### since you want all columns you should give data = biofam[cohort=="1950-1959",]
